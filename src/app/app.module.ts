952a7c727ea63189f2826045459e927c7b95466b
Vincent Alibert <contact.vawec@gmail.com>
2018-02-10 19:35:56 +0100

    création du module home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { OffresModule } from './offres/offres.module';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    HttpModule,
     OffresModule
   ],
   providers: [],
