79b570b9618c44e44df931911e1fea17c460b71e
contact.vawec@gmail.com <contact.vawec@gmail.com>
2018-03-28 08:12:31 +0200

    ajout de l'input pour selectionner une offre

--- src/app/offres/list-offres/list-offres.component.ts
+++ src/app/offres/list-offres/list-offres.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Offre } from '../class/offre';
 import { OffresService } from '../offres.service';
+import { UsersService } from '../../users/users.service';
+import { User } from '../../users/class/user';
 
 
 @Component({
@@ -14,10 +16,12 @@ export class ListOffresComponent implements OnInit {
 
   private offres: Offre[];
   private error: string;
+  private currentUser: User;
 
-  constructor(private offresService: OffresService) { }
+  constructor(private offresService: OffresService, private userService: UsersService) { }
 
   ngOnInit() {
+    this.currentUser = this.userService.currentUser;
     this.offresService.getAllOffres().subscribe(
       data => {
         if (data.status === 'success') {
@@ -31,6 +35,14 @@ export class ListOffresComponent implements OnInit {
         this.error = `Une erreur s'est produite veuillez nous excuser. Erreur : ${error}`;
       }
     );
+  }
+
+  selection(event) {
+    if (event.target.checked) {
+      this.offresService.selectOffre(this.currentUser.idUser, event.target.id).subscribe();
+    } else {
+      this.offresService.unSelectOffre(this.currentUser.idUser, event.target.id).subscribe();
+    }
   }
 
 }
